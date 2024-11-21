8fc3c6dfbd99400448fdd09824082e4f8922003d
rubenci-clase <ruben.corisi@educa.jcyl.es>
2024-11-21 09:12:32 +0100

    Update Semaforo.java

--- src/paquete1/Semaforo.java
+++ src/paquete1/Semaforo.java
@@ -1,15 +1,20 @@
 package paquete1;
 class Semaforo {
     private int contador;
-
+    
+    
     public Semaforo(int contadorInicial) {
         this.contador = contadorInicial;
+        
     }
 
     public synchronized void espera() throws InterruptedException {
         // Decrementamos el contador y comprobamos si es negativo
         while (contador <= 0) {
+        	long startTime = System.nanoTime();
             wait(); // Esperar hasta que haya un recurso disponible
+            long waitTime = System.nanoTime() - startTime; // Tiempo de espera en nanosegundos
+            System.out.println("Tiempo esperando en el bloque synchronized: " + waitTime + " nanosegundos");
         }
         contador--; // Consumir un recurso
     }
@@ -17,5 +22,6 @@ class Semaforo {
     public synchronized void senal() {
         contador++; // Liberar un recurso
         notify(); // Notificar a un hilo en espera, si hay alguno
+        
     }
 }
